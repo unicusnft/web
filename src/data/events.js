2a85856bbeb5f0c250256741d514ce23d6da19b0
JDSanto <javidisanto@gmail.com>
2022-06-15 22:05:02 -0300

    feat: add events.js constant

---
+++ src/data/events.js
@@ -0,0 +1,56 @@
+export const events = [
+  {
+    title: "Duki",
+    type: "Music",
+    location: "Estadio Velez Sarsfield",
+    datetime: new Date(2022, 10, 12, 21, 0),
+    eventImageUrl: "https://static6.ticketek.com.ar/cms_static/sites/default/files/images/show-header/duki_ticketek_960-x-400-4.png",
+    ticketImageUrl: "https://cloudfront-us-east-1.images.arcpublishing.com/infobae/ST5Q635BZZHVTFJ3WN3E7NFF5A.jpg",
+    buyImage1: "https://commons.wikimedia.org/wiki/File:Duko_concierto.jpg",
+    buyImage2: "https://los40es00.epimg.net/los40/imagenes/2019/07/03/musica/1562149419_336088_1562151870_noticia_normal.jpg",
+    nftNumber: 547532,
+    contractId: "0x32fc2d09343ab12c",
+    ticketTypes: [
+      {
+        label: "Tier S - Front Line",
+        price: "25000"
+      },
+      {
+        label: "Tier A - Stalls from 1-50 Lines",
+        price: "20000"
+      },
+      {
+        label: "Tier B - Stalls from 50-100 Lines",
+        price: "16000"
+      },
+      {
+        label: "Tier C - General with Sits",
+        price: "12000"
+      },
+      {
+        label: "Tier D - General Stand",
+        price: "9500"
+      },
+    ],
+    history: [
+      {
+        type: "MINTED",
+        from: "-",
+        to: "Camila Dvorkin",
+        date: new Date(2022, 3, 9, 14, 9, 0)
+      },
+      {
+        type: "RESELL",
+        from: "Camila Dvorkin",
+        to: "Juan Pablo R.",
+        date: new Date(2022, 3, 9, 14, 15, 0)
+      },
+      {
+        type: "MINTED",
+        from: "Juan Pablo R.",
+        to: "Javier Di Santo",
+        date: new Date(2022, 3, 9, 14, 20, 0)
+      },
+    ]
+  },
+]
--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -7,7 +7,8 @@ import {
 import { colors } from "../../core/theme";
 import { Filters } from "./Filters";
 import { EventCard } from "../../components/EventCard.js";
-import { NFTCard } from "../../components/NFTCard.js";
+// import { NFTCard } from "../../components/NFTCard.js";
+import { events } from "../../data/events.js";
 
 export const HomeScreen = () => {
   return (
@@ -16,31 +17,41 @@ export const HomeScreen = () => {
         <Filters />
       </Stack>
       <SimpleGrid minChildWidth='700px' spacing='20px' padding='10px'>
-        <NFTCard
-          title="Duki"
-          type="Music"
-          location="Estadio Velez Sarsfield"
-          datetime={new Date(2022, 10, 12, 21, 0)}
-          imgUrl="https://cloudfront-us-east-1.images.arcpublishing.com/infobae/ST5Q635BZZHVTFJ3WN3E7NFF5A.jpg"
-          nftNumber={547532}/>
-        <EventCard
-          title="Duki"
-          type="Music"
-          location="Estadio Velez Sarsfield"
-          datetime={new Date(2022, 10, 12, 21, 0)}
-          imgUrl="https://static6.ticketek.com.ar/cms_static/sites/default/files/images/show-header/duki_ticketek_960-x-400-4.png"/>
-        <EventCard
-          title="Standup Femenino"
-          type="Theater"
-          location="Complejo Art Media"
-          datetime={new Date(2022, 15, 5, 20, 0)}
-          imgUrl="https://static6.ticketek.com.ar/cms_static/sites/default/files/images/show-header/960_7.png"/>
-        <EventCard
-          title="Duki"
-          type="Music"
-          location="Estadio Velez Sarsfield"
-          datetime={new Date(2022, 15, 5, 20, 0)}
-          imgUrl="https://static6.ticketek.com.ar/cms_static/sites/default/files/images/show-header/duki_ticketek_960-x-400-4.png"/>
+        {
+          events.map(e => <EventCard
+            title={e.title}
+            type={e.type}
+            location={e.location}
+            datetime={e.datetime}
+            imgUrl={e.eventImageUrl}
+            nftNumber={e.nftNumber}
+          />)
+        }
+        {/* <NFTCard */}
+        {/*   title="Duki" */}
+        {/*   type="Music" */}
+        {/*   location="Estadio Velez Sarsfield" */}
+        {/*   datetime={new Date(2022, 10, 12, 21, 0)} */}
+        {/*   imgUrl="https://cloudfront-us-east-1.images.arcpublishing.com/infobae/ST5Q635BZZHVTFJ3WN3E7NFF5A.jpg" */}
+        {/*   nftNumber={547532}/> */}
+        {/* <EventCard */}
+        {/*   title="Duki" */}
+        {/*   type="Music" */}
+        {/*   location="Estadio Velez Sarsfield" */}
+        {/*   datetime={new Date(2022, 10, 12, 21, 0)} */}
+        {/*   imgUrl="https://static6.ticketek.com.ar/cms_static/sites/default/files/images/show-header/duki_ticketek_960-x-400-4.png"/> */}
+        {/* <EventCard */}
+        {/*   title="Standup Femenino" */}
+        {/*   type="Theater" */}
+        {/*   location="Complejo Art Media" */}
+        {/*   datetime={new Date(2022, 15, 5, 20, 0)} */}
+        {/*   imgUrl="https://static6.ticketek.com.ar/cms_static/sites/default/files/images/show-header/960_7.png"/> */}
+        {/* <EventCard */}
+        {/*   title="Duki" */}
+        {/*   type="Music" */}
+        {/*   location="Estadio Velez Sarsfield" */}
+        {/*   datetime={new Date(2022, 15, 5, 20, 0)} */}
+        {/*   imgUrl="https://static6.ticketek.com.ar/cms_static/sites/default/files/images/show-header/duki_ticketek_960-x-400-4.png"/> */}
       </SimpleGrid>
     </Box>
   );
