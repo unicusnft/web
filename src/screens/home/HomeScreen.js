dab5117490af4da3b87e4235e5db85ba576e0770
Francisco <fnasif@fi.uba.ar>
2022-06-16 18:14:35 -0300

    Page Titles

--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -1,26 +1,33 @@
 import React, { useState } from "react";
-import {
-  Stack,
-  Wrap,
-  WrapItem,
-  Box
-} from "@chakra-ui/react";
+import { Stack, Wrap, WrapItem, Box, Text } from "@chakra-ui/react";
 import { Filters } from "../../components/Filters.js";
 import { EventCard } from "../../components/EventCard.js";
 import { Toolbar } from "../../components/Toolbar";
 import { events } from "../../data/events.js";
 
+const TitlePageStyle = {
+  fontSize: "25px",
+  fontWeight: "bold",
+  margin: "20px",
+};
+
 export const HomeScreen = () => {
   const [event, setEvent] = useState("");
   return (
     <>
-      <Toolbar/>
+      <Toolbar />
       <Box backgroundColor="#121212">
-        <Stack alignItems="center" px={4} mt={6}>
-          <Filters event={event} setEvent={setEvent} description="Buscar un evento"/>
+        <Stack alignItems="center">
+          <Text sx={TitlePageStyle}>Eventos</Text>
         </Stack>
-
-        <br/>
+        <Stack alignItems="center" px={4}>
+          <Filters
+            event={event}
+            setEvent={setEvent}
+            description="Buscar un evento"
+          />
+        </Stack>
+        <br />
         <Wrap spacing="25px" justify="center">
           {events
             .filter(
@@ -32,7 +39,7 @@ export const HomeScreen = () => {
                     .includes(event.toLocaleLowerCase()))
             )
             .map((e) => (
-              <WrapItem   key={e.title}>
+              <WrapItem key={e.title}>
                 <EventCard
                   id={e.id}
                   title={e.title}
--- src/screens/my-tickets/MyTickets.js
+++ src/screens/my-tickets/MyTickets.js
@@ -1,46 +1,59 @@
-import React, {useState} from "react";
-import {Container, Stack, Wrap, WrapItem} from "@chakra-ui/react";
-import {Toolbar} from "../../components/Toolbar";
+import React, { useState } from "react";
+import { Container, Stack, Wrap, WrapItem, Text } from "@chakra-ui/react";
+import { Toolbar } from "../../components/Toolbar";
 
-import {NFTCard} from "../../components/NFTCard";
-import {Filters} from "../../components/Filters";
-import {events} from "../../data/events";
+import { NFTCard } from "../../components/NFTCard";
+import { Filters } from "../../components/Filters";
+import { events } from "../../data/events";
+
+const TitlePageStyle = {
+  fontSize: "25px",
+  fontWeight: "bold",
+  margin: "20px",
+};
 
 export const MyTickets = () => {
-    const [event, setEvent] = useState("");
-    return (
-        <>
-            <Toolbar title="Mis tickets"/>
-            <Stack alignItems="center" px={4} mt={6}>
-                <Filters event={event} setEvent={setEvent} description="Buscar en mis tickets"/>
-            </Stack>
-            <br/>
-            <Container maxW='8xl' padding='0px'>
-                <Wrap spacing="25px" justify="center">
-                    {events
-                        .filter(
-                            (x) =>
-                                x.owned && (event === "" ||
-                                (event !== "" &&
-                                    x.title
-                                        .toLocaleLowerCase()
-                                        .includes(event.toLocaleLowerCase())))
-                        )
-                        .map(e => (
-                            <WrapItem key={e.title}>
-                                <NFTCard
-                                    title={e.title}
-                                    type={e.type}
-                                    location={e.location}
-                                    datetime={e.datetime}
-                                    imgUrl={e.ticketImageUrl}
-                                    nftNumber={e.nftNumber}
-                                />
-                            </WrapItem>
-                        ))}
-                </Wrap>
-
-            </Container>
-        </>
-    )
-}
+  const [event, setEvent] = useState("");
+  return (
+    <>
+      <Toolbar title="Mis tickets" />
+      <Stack alignItems="center">
+        <Text sx={TitlePageStyle}>Mis tickets</Text>
+      </Stack>
+      <Stack alignItems="center" px={4}>
+        <Filters
+          event={event}
+          setEvent={setEvent}
+          description="Buscar en mis tickets"
+        />
+      </Stack>
+      <br />
+      <Container maxW="8xl" padding="0px">
+        <Wrap spacing="25px" justify="center">
+          {events
+            .filter(
+              (x) =>
+                x.owned &&
+                (event === "" ||
+                  (event !== "" &&
+                    x.title
+                      .toLocaleLowerCase()
+                      .includes(event.toLocaleLowerCase())))
+            )
+            .map((e) => (
+              <WrapItem key={e.title}>
+                <NFTCard
+                  title={e.title}
+                  type={e.type}
+                  location={e.location}
+                  datetime={e.datetime}
+                  imgUrl={e.ticketImageUrl}
+                  nftNumber={e.nftNumber}
+                />
+              </WrapItem>
+            ))}
+        </Wrap>
+      </Container>
+    </>
+  );
+};
