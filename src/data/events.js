755b1167859bc9cd44346873fc7ac6dbb45897c3
Cami Dvorkin <camidvorkin@fb.com>
2022-06-16 12:49:45 -0300

    Add new events with images

--- src/data/events.js
+++ src/data/events.js
@@ -1,37 +1,37 @@
 export const events = [
   {
     id: 1,
-    title: "Duki",
-    type: "Music",
-    location: "Estadio Velez Sarsfield",
-    datetime: new Date(2022, 10, 12, 21, 0),
-    eventImageUrl: "https://static6.ticketek.com.ar/cms_static/sites/default/files/images/show-header/duki_ticketek_960-x-400-4.png",
-    ticketImageUrl: "https://cloudfront-us-east-1.images.arcpublishing.com/infobae/ST5Q635BZZHVTFJ3WN3E7NFF5A.jpg",
-    buyImage1: "https://upload.wikimedia.org/wikipedia/commons/9/98/Duko_concierto.jpg",
-    buyImage2: "https://los40es00.epimg.net/los40/imagenes/2019/07/03/musica/1562149419_336088_1562151870_noticia_normal.jpg",
+    title: "Ted Talk with Elon Musk and Agustin Perrotta",
+    type: "Tech",
+    location: "Costa Salguero",
+    datetime: new Date(2022, 10, 10, 17, 0),
+    eventImageUrl: "/images/tedtalk1.jpg",
+    ticketImageUrl: "/images/tedtalk2.jpg",
+    buyImage1: "/images/tedtalk3.jpg",
+    buyImage2: "/images/tedtalk4.jpg",
     nftNumber: 547532,
     owned: true,  // if owned == true, it will appear in the NFT ticket page
     contractId: "0x32fc2d09343ab12c",
     ticketTypes: [
       {
-        label: "Tier S - Front Line",
+        label: "Tier S - Front Line + Meet & Greet",
         price: "25000"
       },
       {
         label: "Tier A - Stalls from 1-50 Lines",
-        price: "20000"
+        price: "9000"
       },
       {
         label: "Tier B - Stalls from 50-100 Lines",
-        price: "16000"
+        price: "7000"
       },
       {
         label: "Tier C - General with Sits",
-        price: "12000"
+        price: "5000"
       },
       {
         label: "Tier D - General Stand",
-        price: "9500"
+        price: "3000"
       },
     ],
     history: [
@@ -54,24 +54,119 @@ export const events = [
         date: new Date(2022, 3, 9, 14, 9, 0)
       }
     ]
-  },
-  {
+  },  {
     id: 2,
-    title: "Boca vs River",
+    title: "Argentina vs Italia",
     type: "Sports",
-    location: "La Bombonera",
-    datetime: new Date(2022, 5, 23, 20, 0),
-    eventImageUrl: "https://images.ole.com.ar/2021/07/19/9chV9ETfI_1256x620__2.jpg#1626727436151",
-    ticketImageUrl: "https://www.telediariodigital.net/wp-content/uploads/2018/11/Boca-River.jpg",
-    buyImage1: "https://www.ole.com.ar/futbol-primera/boca-river-final-publico_0_TlqoDg8qVL.html",
-    buyImage2: "https://www.ole.com.ar/futbol-primera/boca-ranking-titulos-copa-argentina_0_XSrXiXNic.html",
+    location: "River",
+    datetime: new Date(2022, 10, 23, 21, 0),
+    eventImageUrl: "/images/finalissima1.jpeg",
+    ticketImageUrl: "/images/finalissima2.jpeg",
+    buyImage1: "/images/finalissima1.jpeg",
+    buyImage2: "/images/finalissima3.jpeg",
     nftNumber: 547573,
     owned: true,
     contractId: "0x32fc2d09343ad54f",
     ticketTypes: [
       {
-        label: "Tier S - Front Line",
-        price: "25000"
+        label: "Platea Baja",
+        price: "12000"
+      },
+      {
+        label: "Platea Media",
+        price: "9000"
+      },
+      {
+        label: "Platea Alta",
+        price: "5000"
+      },
+      {
+        label: "Popular",
+        price: "2500"
+      },
+    ],
+    history: [
+      {
+        type: "MINTED",
+        from: "-",
+        to: "Renzo Jacinto",
+        date: new Date(2022, 3, 9, 14, 9, 0)
+      },
+    ]
+  },
+  {
+    id: 3,
+    title: "LollaPalooza",
+    type: "Music",
+    location: "Hipódromo de San Isidro",
+    datetime: new Date(2023, 3, 10, 12, 0),
+    eventImageUrl: "/images/lolla1.jpeg",
+    ticketImageUrl: "/images/lolla2.jpeg",
+    buyImage1: "/images/lolla3.jpeg",
+    buyImage2: "/images/lolla4.jpeg",
+    nftNumber: 654542,
+    owned: true,
+    contractId: "0x3fdp6s789343ad54f",
+    ticketTypes: [
+      {
+        label: "Pack 3 días",
+        price: "28000"
+      },
+      {  
+        label: "Día 1",
+        price: "13000"
+      },
+      {
+        label: "Día 2",
+        price: "14000"
+      },
+      {
+        label: "Día 3",
+        price: "14000"
+      },
+    ],
+    history: [
+      {
+        type: "MINTED",
+        from: "-",
+        to: "Juan Pablo Rombola",
+        date: new Date(2022, 3, 9, 14, 9, 0)
+      },
+    ]
+  },
+  {
+    id: 4,
+    title: "COLDPLAY",
+    type: "Music",
+    location: "River",
+    datetime: new Date(2022, 11, 5, 20, 0),
+    eventImageUrl: "/images/coldplay1.jpeg",
+    ticketImageUrl: "/images/coldplay2.jpeg",
+    buyImage1: "/images/coldplay3.jpeg",
+    buyImage2: "/images/coldplay4.jpeg",
+    nftNumber: 547573,
+    owned: true,
+    contractId: "0x32fc2d09343ad54f",
+    ticketTypes: [
+      {
+        label: "Campo delantero",
+        price: "19500"
+      },
+      {  
+        label: "Platea preferida",
+        price: "19000"
+      },
+      {
+        label: "Platea",
+        price: "13500"
+      },
+      {
+        label: "Platea Media",
+        price: "12000"
+      },
+      {
+        label: "Campo",
+        price: "10000"
       },
     ],
     history: [
@@ -81,6 +176,134 @@ export const events = [
         to: "Camila Dvorkin",
         date: new Date(2022, 3, 9, 14, 9, 0)
       },
+    ]
+  },
+  {
+    id: 5,
+    title: "Boca vs River",
+    type: "Sports",
+    location: "La Bombonera",
+    datetime: new Date(2022, 5, 23, 20, 0),
+    eventImageUrl: "/images/boke1.jpeg",
+    ticketImageUrl: "/images/boke2.jpeg",
+    buyImage1: "/images/boke3.jpeg",
+    buyImage2: "/images/boke4.jpeg",
+    nftNumber: 547573,
+    owned: true,
+    contractId: "0x32fc2d09343ad54f",
+    ticketTypes: [
+      {
+        label: "Platea Baja",
+        price: "8000"
+      },
+      {
+        label: "Platea Media",
+        price: "7000"
+      },
+      {
+        label: "Platea Alta",
+        price: "4000"
+      },
+      {
+        label: "Popular",
+        price: "2500"
+      },
+    ],
+    history: [
+      {
+        type: "MINTED",
+        from: "-",
+        to: "Fransisco Nasif",
+        date: new Date(2022, 3, 9, 14, 9, 0)
+      },
+    ]
+  },
+  {
+    id: 6,
+    title: "Fito Paez",
+    type: "Music",
+    location: "Velez",
+    datetime: new Date(2022, 11, 23, 20, 0),
+    eventImageUrl: "/images/fito1.jpeg",
+    ticketImageUrl: "/images/fito2.jpeg",
+    buyImage1: "/images/fito3.jpeg",
+    buyImage2: "/images/fito4.jpeg",
+    nftNumber: 547323,
+    owned: true,
+    contractId: "0x335dd094524ad54f",
+    ticketTypes: [
+      {
+        label: "Campo delantero",
+        price: "9500"
+      },
+      {  
+        label: "Platea preferida",
+        price: "9000"
+      },
+      {
+        label: "Platea",
+        price: "7000"
+      },
+      {
+        label: "Platea Media",
+        price: "5000"
+      },
+      {
+        label: "Campo",
+        price: "5000"
+      },
+    ],
+    history: [
+      {
+        type: "MINTED",
+        from: "-",
+        to: "Javier Di Santo",
+        date: new Date(2022, 3, 9, 14, 9, 0)
+      },
+    ]
+  },
+  {
+    id: 6,
+    title: "Duki",
+    type: "Music",
+    location: "Velez",
+    datetime: new Date(2022, 10, 10, 17, 0),
+    eventImageUrl: "/images/Duki1.png",
+    ticketImageUrl: "/images/Duki2.png",
+    buyImage1: "/images/Duki3.jpeg",
+    buyImage2: "/images/Duki4.jpeg",
+    nftNumber: 547532,
+    owned: true,  // if owned == true, it will appear in the NFT ticket page
+    contractId: "0x32fc2d09343ab12c",
+    ticketTypes: [
+      {
+        label: "Tier S - Front Line + Meet & Greet",
+        price: "25000"
+      },
+      {
+        label: "Tier A - Stalls from 1-50 Lines",
+        price: "9000"
+      },
+      {
+        label: "Tier B - Stalls from 50-100 Lines",
+        price: "7000"
+      },
+      {
+        label: "Tier C - General with Sits",
+        price: "5000"
+      },
+      {
+        label: "Tier D - General Stand",
+        price: "3000"
+      },
+    ],
+    history: [
+      {
+        type: "TRANSFER",
+        from: "Juan Pablo R.",
+        to: "Javier Di Santo",
+        date: new Date(2022, 3, 9, 14, 20, 0)
+      },
       {
         type: "RESELL",
         from: "Camila Dvorkin",
@@ -89,10 +312,10 @@ export const events = [
       },
       {
         type: "MINTED",
-        from: "Juan Pablo R.",
-        to: "Javier Di Santo",
-        date: new Date(2022, 3, 9, 14, 20, 0)
-      },
+        from: "-",
+        to: "Camila Dvorkin",
+        date: new Date(2022, 3, 9, 14, 9, 0)
+      }
     ]
   },
 ]
