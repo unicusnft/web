ec5799df63555d2dbef1f8711b472e023ffb64d8
Francisco <fnasif@fi.uba.ar>
2022-07-13 20:59:30 -0300

    Blur and text on QR

--- src/components/Cards/TicketValidate.js
+++ src/components/Cards/TicketValidate.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import QRCode from "qrcode";
-import { Image } from "@chakra-ui/react";
+import { Image, Text } from "@chakra-ui/react";
 import { BASE_URL, VALIDATE_NFT } from "../../services/Utils";
 import "../../styles/card.css";
 
@@ -31,8 +31,32 @@ const TicketValidate = ({ nftId, urlImage }) => {
             rounded={40}
           />
         </div>
-        <div className="flip-card-back" background={qr}>
-          <Image src={qr} alt="Ticket QR" objectFit="cover" rounded={40} />
+        <div className="flip-card-back">
+          <Image
+            src={urlImage}
+            alt="Ticket photo"
+            w="350px"
+            h="400px"
+            objectFit="cover"
+            rounded={40}
+            filter="blur(8px)"
+          />
+          <Image
+            src={qr}
+            alt="Ticket QR"
+            objectFit="cover"
+            rounded={40}
+            style={{ position: "absolute" }}
+            mb="125px"
+          />
+          <Text
+            fontSize="2xl"
+            mb="25px"
+            style={{ position: "absolute" }}
+            as="strong"
+          >
+            Escaneá el código para ingresar al evento!
+          </Text>
         </div>
       </div>
     </div>
