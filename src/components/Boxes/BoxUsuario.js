4a6c9cfbf93be58c771bf181643107c95f74416f
Francisco <fnasif@fi.uba.ar>
2022-07-14 12:01:27 -0300

    Tag Validated Ticket

--- src/components/Boxes/BoxUsuario.js
+++ src/components/Boxes/BoxUsuario.js
@@ -1,17 +1,18 @@
 import React from "react";
-import {Avatar, Box, Text, Badge} from "@chakra-ui/react";
-import {CheckIcon} from "@chakra-ui/icons";
+import { Avatar, Box, Text, Badge } from "@chakra-ui/react";
+import { CheckIcon } from "@chakra-ui/icons";
+import { colors } from "../../core/theme";
 
 const BoxUsuario = ({
-                      full_name: name,
-                      username,
-                      profile_image_url: avatar,
-                      esAmigo,
-                      isSuccess,
-                    }) => {
+  full_name: name,
+  username,
+  profile_image_url: avatar,
+  esAmigo,
+  isSuccess,
+}) => {
   return (
     <>
-      <Avatar src={avatar}/>
+      <Avatar src={avatar} />
       <Box ml="3" width="100%">
         <Text fontWeight="bold" align="start">
           {name}
@@ -23,7 +24,7 @@ const BoxUsuario = ({
         </Text>
         <Text fontSize="sm" display="flex" justifyContent="space-between">
           {username}
-          {isSuccess && <CheckIcon color="#00FF5F" boxSize={5}/>}
+          {isSuccess && <CheckIcon color={colors.success} boxSize={5} />}
         </Text>
       </Box>
     </>
